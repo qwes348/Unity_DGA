b2ad3f989cf8fa4812b1c21ac22baf68e076e994
Hwangk <qwes348@naver.com>
2019-04-26 01:39:03 +0900

    Unity

--- Day03_Roaming_Input/Assets/Roaming.cs
+++ Day03_Roaming_Input/Assets/Roaming.cs
@@ -54,8 +54,9 @@ public class Roaming : MonoBehaviour
         //transform.position = transform.position + dir.normalized * moveSpeed * Time.deltaTime;
         //transform.Translate(dir.normalized * moveSpeed * Time.deltaTime, Space.World);
         //transform.position = Vector3.Lerp(transform.position, nextPoint, moveSpeed * Time.deltaTime);   // 처음에는 빠르고 갈수록 느려짐(계속 이동하는 현재위치에서 구하기때문)
-        //transform.position = Vector3.Lerp(transform.position, nextPoint, moveSpeed * Time.deltaTime / dir.magnitude);      // 등속Ver.
-        transform.position = Vector3.MoveTowards(transform.position, nextPoint, moveSpeed * Time.deltaTime);      // MoveTowards 가장 실용적
+        transform.position = Vector3.Lerp(transform.position, nextPoint, Time.deltaTime / dir.magnitude);      // 등속Ver.
+        print((moveSpeed * Time.deltaTime / dir.magnitude).ToString());
+        /*transform.position = Vector3.MoveTowards(transform.position, nextPoint, moveSpeed * Time.deltaTime);*/      // MoveTowards 가장 실용적
         //transform.position = Vector3.Slerp(transform.position, nextPoint, moveSpeed * Time.deltaTime);      // Sphere Lerp        
     }
 }
