71855f6063e364d463ffe3c1d364301cce50d748
Hwngk <qwes348@naver.com>
2019-04-29 19:38:32 +0900

    Unity

---
+++ Day06_Coroutine/Assets/Selection_Co.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection_Co : MonoBehaviour
+{
+    public float angle = 90f;
+    public float duration = 1f;
+
+    bool isRotating = false;
+    float remainingAngle;
+    float remainingDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isRotating)
+        {
+            StartCoroutine(Selection());
+        }
+    }
+
+    IEnumerator Selection()
+    {
+        if (!isRotating)
+        {
+            isRotating = true;
+            remainingAngle = angle;
+            remainingDuration = duration;
+        }
+        if (isRotating)
+        {
+            while (isRotating)
+            {
+                float anglePerFrame = (remainingAngle / remainingDuration) * Time.deltaTime; // 한프레임의 회전값
+                if (remainingAngle < anglePerFrame)
+                {
+                    anglePerFrame = remainingAngle;
+                    isRotating = false;
+                }
+                transform.Rotate(Vector3.up * anglePerFrame);
+
+                remainingAngle -= anglePerFrame;
+                remainingDuration -= Time.deltaTime;
+                yield return null;
+
+            }
+        }
+    }
+}
---
+++ HW_BossPattern/Assets/Jump_Pattern.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump_Pattern : MonoBehaviour
+{
+    bool isMoving;
+    Rigidbody rb;
+    int jumpCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && !isMoving)
+        {
+            StartCoroutine(JumpPattern());
+        }
+    }
+
+    IEnumerator JumpPattern()
+    {
+        if (!isMoving)
+        {
+            rb.AddForce(Vector3.up * 300f);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.transform.name == "Plane")
+            isMoving = false;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        isMoving = true;
+    }
+}
