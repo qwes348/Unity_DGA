0b2eb4a3c7c613777377e253e270bf5982f67e2e
Hwngk <qwes348@naver.com>
2019-05-07 12:52:04 +0900

    Unity

---
+++ Day10_FPS/Assets/Gun.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float fireRate = 10;
+    public Light muzzleFlash;
+
+    Camera fpsCamera;
+    float nextTimeToFire = 0f;
+    Vector3 originPos, smoothVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fpsCamera = GetComponentInParent<Camera>();
+        originPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;   // 초당 10발을 쏠수있다
+            Shoot();
+        }
+        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, originPos, ref smoothVel, 0.1f);  // lerp함수와 비슷, 스프링처럼 동작함
+    }
+
+    private void Shoot()
+    {
+        // 총구화염
+        muzzleFlash.enabled = true;
+        Invoke("OffFlashLight", 0.05f);  // 코루틴없이 시간을주는 Invoke
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCamera.transform.position, fpsCamera.transform.forward, out hit, 200f))  // 총에서부터가 아니라 카메라에서 쏨
+        {
+            print(hit.transform.name);            
+            Debug.DrawLine(fpsCamera.transform.position, hit.point, Color.magenta, 2f);
+            if(hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(fpsCamera.transform.forward * 500f);
+            }
+        }
+
+
+        // 반동구현
+        transform.localPosition -= Vector3.forward * UnityEngine.Random.Range(0.07f, 0.3f);
+    }
+
+    void OffFlashLight()
+    {
+        muzzleFlash.enabled = false;
+    }
+}
---
+++ Day10_FPS/Assets/HealOverTime.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealOverTime : MonoBehaviour
+{
+    public GameObject healFX;
+    bool isHealing = false;
+    GameObject fx;
+
+    public void Heal()
+    {
+        if (!isHealing)
+        {
+            fx = Instantiate(healFX, transform.Find("FXPos"));
+            isHealing = true;
+            Invoke("RemoveHealFX", 1.9f);  // 1.9초뒤에 RemoveHealFX 함수 실행
+        }
+    }
+
+    void RemoveHealFX()
+    {
+        Destroy(fx);
+        isHealing = false;
+    }
+}
