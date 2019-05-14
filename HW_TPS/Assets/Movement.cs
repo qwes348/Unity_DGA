9171c4332086ef058330668497c2aa7d80889f04
Hwangk <qwes348@naver.com>
2019-05-15 01:40:44 +0900

    HW

---
+++ HW_TPS/Assets/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    Animator anim;
+    Vector3 moveDirection = Vector3.zero;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        moveDirection = (new Vector3(0, 0, v));
+        if (moveDirection == Vector3.zero)
+            anim.SetFloat("Speed", -1f);
+        else
+            anim.SetFloat("Speed", 1f);
+
+        transform.LookAt(transform.position + moveDirection);
+        moveDirection *= moveSpeed;
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 move = moveDirection * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + move);
+    }
+}
---
+++ HW_TPS/Assets/PlayerFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollow : MonoBehaviour
+{
+    public Transform target;
+    public float height;
+
+    float distance = -10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x, height, target.position.z - distance);
+        transform.LookAt(target);
+        transform.position = newPos;
+    }
+}
