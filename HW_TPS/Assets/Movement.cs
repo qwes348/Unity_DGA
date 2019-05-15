18e38a4759d152177428beda392a5925fe244c69
Hwangk <qwes348@naver.com>
2019-05-16 01:01:19 +0900

    HW

--- HW_TPS/Assets/Movement.cs
+++ HW_TPS/Assets/Movement.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 7f;
+    public float turnSpeed = 100f;
 
     Animator anim;
     Vector3 moveDirection = Vector3.zero;
+    Vector3 newRot = Vector3.zero;
+
+    bool mouse_R = false;
+    bool isMoving = false;
+    float v, h;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
@@ -19,22 +25,79 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        v = Input.GetAxisRaw("Vertical");
+        h = Input.GetAxisRaw("Horizontal");
+        
+        if(Input.GetMouseButton(1))
+        {
+            anim.SetBool("Walk_F", true);
+            mouse_R = true;
+            transform.Translate(new Vector3(h, 0, 0) * moveSpeed * Time.deltaTime, Space.Self);
+        }
+        if(Input.GetMouseButtonUp(1))
+        {
+            mouse_R = false;
+            anim.SetBool("Walk_F", false);            
+        }
+
+        WalkAnim();
+        if (!isMoving && !mouse_R)
+            TurnAnim();
+    }
+
+    public void TurnAnim()
+    {
+        
+
+        if (h == 1)  
+        {
+            anim.SetTrigger("Turn_R");
+            newRot = rb.rotation.eulerAngles;
+            newRot.y += turnSpeed * Time.fixedDeltaTime;
+        }
+        else if(h == -1) 
+        {
+            anim.SetTrigger("Turn_L");
+            newRot = rb.rotation.eulerAngles;
+            newRot.y -= turnSpeed * Time.fixedDeltaTime;
+        }
+        //if (newRot.y >= 180f)
+        //    newRot.y -= 360f;
+        //newRot.y = Mathf.Clamp(newRot.y, -90f, 90f);
+    }
+
+    public void WalkAnim()
+    {
+                
         moveDirection = (new Vector3(0, 0, v));
-        if (moveDirection == Vector3.zero)
-            anim.SetFloat("Speed", -1f);
-        else
-            anim.SetFloat("Speed", 1f);
+        if (v == 0)  // 정지
+        {
+            isMoving = false;
+            anim.SetBool("Walk_F", false);
+            anim.SetBool("Walk_B", false);
+        }
+        else if (v == 1)  // 앞걸음
+        {
+            isMoving = true;
+            anim.SetBool("Walk_F", true);
+        }
+        else  // 뒷걸음
+        {
+            isMoving = true;
+            anim.SetBool("Walk_B", true);
+        }
 
-        transform.LookAt(transform.position + moveDirection);
         moveDirection *= moveSpeed;
-
     }
 
     private void FixedUpdate()
     {
         Vector3 move = moveDirection * Time.fixedDeltaTime;
-        rb.MovePosition(transform.position + move);
+        //rb.MovePosition(rb.position + move);      // 항상 누르는 방향을 바라보는것이 아니기때문에 작동안함
+        transform.Translate(moveDirection * Time.fixedDeltaTime, Space.Self);
+        
+        if(h != 0)
+            rb.MoveRotation(Quaternion.Euler(newRot));
+        //rb.rotation = Quaternion.Slerp(rb.rotation, Quaternion.Euler(newRot), turnSpeed*Time.fixedDeltaTime);
     }
 }
--- HW_TPS/Assets/PlayerFollow.cs
+++ HW_TPS/Assets/PlayerFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerFollow : MonoBehaviour
 {
+    Movement movement;
     public Transform target;
     public float height;
 
@@ -12,14 +13,17 @@ public class PlayerFollow : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0f, height, distance);
+        transform.rotation = Quaternion.Euler(45f, 0f, 0f);
+        movement = gameObject.GetComponentInParent<Movement>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = new Vector3(target.position.x, height, target.position.z - distance);
-        transform.LookAt(target);
-        transform.position = newPos;
+        //Vector3 newPos = new Vector3(target.position.x, height, target.position.z + distance);
+        ////Vector3 newRot = new Vector3(45f, target.rotation.y, transform.localRotation.z);        
+        //transform.position = newPos;
+        //transform.rotation = Quaternion.Euler(newRot * movement.turnSpeed * Time.fixedDeltaTime);
     }
 }
