081521d1dfb0a52ed0c5e736d1782b982706029d
Hwngk <qwes348@naver.com>
2019-06-03 17:18:38 +0900

    Unity_DGA_Tips

---
+++ Day29_UnityTips_Attribute_Singleton_pooling/Assets/PoolTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using EZObjectPools;
+
+public class PoolTest : MonoBehaviour
+{
+    public EZObjectPool shellPool;
+
+    GameObject shell;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(shellPool.TryGetNextObject(Vector3.zero, Quaternion.identity, out shell))
+        {
+            print(shellPool.AvailableObjectCount() == 9); // pool에 남은 오브젝트 수
+            print(shellPool.ActiveObjectCount() == 1);  // pool에서 나온 오브젝트 수
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print(shellPool.AvailableObjectCount() == 9); // pool에 남은 오브젝트 수
+            print(shellPool.ActiveObjectCount() == 1);  // pool에서 나온 오브젝트 수
+            shell.SetActive(false);  // pool에 반납
+            print(shellPool.AvailableObjectCount() == 10); // pool에 남은 오브젝트 수
+            print(shellPool.ActiveObjectCount() == 0);  // pool에서 나온 오브젝트 수
+        }
+    }
+}
---
+++ Day29_UnityTips_Attribute_Singleton_pooling/Assets/Shell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shell : MonoBehaviour
+{
+    private void OnEnable()  // 삭제가아닌 껏다켰다 하는식이기 때문에 여기서 초기화해줘야함
+        // 대표적으로는 총알의 Rigidbody의 Velocity값을 여기서 초기화시켜줘야함
+    {
+        print("OnEnable");
+    }
+
+    private void OnDisable()
+    {
+        print("OnDisable");
+    }
+}
