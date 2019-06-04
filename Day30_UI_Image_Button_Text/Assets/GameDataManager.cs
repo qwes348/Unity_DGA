35b02f26602d8a1e010c1411b54d5f50e849597c
Hwngk <qwes348@naver.com>
2019-06-04 15:32:19 +0900

    Unity_DGA

---
+++ Day30_UI_Image_Button_Text/Assets/GameDataManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataManager : Singleton<GameDataManager>
+{
+    protected GameDataManager() { }  // private와 동일하지만 상속받은 자식은 쓸수있다.
+
+    float currentHealth = 100f;
+    float maxHealth = 100f;
+    int timeCount = 99;
+
+    int timeStamp = 0;  // 값을 바꾸는 모든함수에서 timeStamp를 업데이트해서 기록을 남긴다
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0f)
+            currentHealth = 0f;
+        UpdateTimeStamp();
+    }
+
+    public void Heal(float amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        UpdateTimeStamp();
+    }
+
+    public float GetCurrentHealth()
+    {
+        return currentHealth;
+    }
+    public float GetMaxHealth()
+    {
+        return maxHealth;
+    }
+    
+    public int GetTimeCount()
+    {
+        return timeCount;
+    }
+
+    public void SetTimeCount(int t)
+    {
+        timeCount = t;
+        UpdateTimeStamp();
+    }
+
+    void UpdateTimeStamp()  // 값을바꾸는 작업을하면 호출하는 함수
+    {
+        timeStamp++;
+        if (timeStamp <= 0)
+            timeStamp = 1;
+    }
+    public int GetTimeStamp()
+    {
+        return timeStamp;
+    }
+}
---
+++ Day30_UI_Image_Button_Text/Assets/UIController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    public Health leftSide;
+    public Button damageButton;
+    public Button healButton;
+    public Text timerText;
+
+    int uiTimeStamp = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageButton.onClick.AddListener(() => GameDataManager.Instance.TakeDamage(10));
+        healButton.onClick.AddListener(() => GameDataManager.Instance.Heal(20));
+    }
+
+    // Update is called once per frame
+    void Update()  // MVC == 모델 뷰 컨트롤러
+    {
+        int timeStamp = GameDataManager.Instance.GetTimeStamp();
+        if(timeStamp != uiTimeStamp)  // timeStamp가 바뀌었다면 모든UI를 업데이트한다 Polling 이라고함
+        {
+            uiTimeStamp = timeStamp;
+            float currentHealth = GameDataManager.Instance.GetCurrentHealth();
+            float maxHealth = GameDataManager.Instance.GetMaxHealth();
+            leftSide.UpdateHealthBart(currentHealth, maxHealth);
+        }
+    }
+}
