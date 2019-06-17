2a83f6154cfc2f828596382fc52de61117cb2043
Hwngk <qwes348@naver.com>
2019-06-17 13:11:38 +0900

    Unity_DGA

--- Day33_AutoLayout/Assets/Readme.cs
+++ Day33_AutoLayout/Assets/Readme.cs
@@ -21,9 +21,9 @@ public class Readme : MonoBehaviour
  *  Layout Element
  *  1. 객체의 크기가 이렇게되면 좋겠다 라는 정보를 제공, 기종 정보를 override 시킴
  *  2. Min, Preferred, Flexible
- *  3. Flexible : 단위가 ratio(weight) == 가중치
+ *  3. Flexible : 단위가 ratio(weight) == 가중치(비율)
  *  4. Image/Text Component는 prefferred 값이 자동으로 설정된다. 단 실제 Texture가 할당된 경우만 가능
- *      예를들어 Image의 Texture 없이 색상만 넣었다면 prefferred값은 0이다 즉 존재하지 않게됨
+ *      예를들어 Image의 Texture 없이 색상만 넣었다면 prefferred값은 0이다 즉 존재하지 않게됨(현물이 없음)
  *   5. Flexible이 제대로 동작하려면 부모의 Child Force Expand를 Off해야 한다. (서로 상반되는 개념이다)
  *   */
 
