acf202d926fce5607a143b39e6feb2257a118e2c
Hwngk <qwes348@naver.com>
2019-06-19 12:23:54 +0900

    Unity_DGA

---
+++ Day36_HTTP_Json/Assets/DownloadTexture.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DownloadTexture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetTextureFromURL());
+    }
+
+    IEnumerator GetTextureFromURL()
+    {
+        // http :80 , https :443 기본포트
+        string url = "https://avatars3.githubusercontent.com:443/u/48681306?s=460&v=4";
+
+        // way 1
+        using (UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url))  
+        {
+            yield return uwr.SendWebRequest();
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                print(uwr.error);
+            }
+            else
+            {
+                var texture = DownloadHandlerTexture.GetContent(uwr);
+                GetComponent<Renderer>().material.mainTexture = texture;
+            }
+        }
+
+        // way 2
+        //{
+        //    UnityWebRequest uwr = UnityWebRequestTexture.GetTexture(url); 
+        //    try
+        //    {
+        //        yield return uwr.SendWebRequest();
+        //        if (uwr.isNetworkError || uwr.isHttpError)
+        //        {
+        //            print(uwr.error);
+        //        }
+        //        else
+        //        {
+        //            var texture = DownloadHandlerTexture.GetContent(uwr);
+        //            GetComponent<Renderer>().material.mainTexture = texture;
+        //        }
+        //    }
+        //    finally
+        //    {
+        //        if (uwr != null)
+        //            uwr.Dispose();   // 메모리에서 제거
+        //        // 바로 제거해주는 이유는 OS측에서 중요한 자원이기 때문에 안쓸거라면 바로바로 반납해줘야한다
+        //    }
+        //}
+
+        // Way 3 == legacy
+        //using (WWW www = new WWW(url))   
+        //{
+        //    yield return www;
+        //    GetComponent<Renderer>().material.mainTexture = www.texture;
+        //}
+    }
+}
---
+++ Day36_HTTP_Json/Assets/HandloingJson.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class HandloingJson : MonoBehaviour
+{
+    string pathJson;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathJson = Application.persistentDataPath + "/gameScore.json";
+        print(pathJson);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            SaveJsonFile();
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadJsonFile();
+        }
+    }
+
+    private void LoadJsonFile()
+    {
+        if(File.Exists(pathJson))
+        {
+            string dataAsJson = File.ReadAllText(pathJson);
+            GameScore newGS = JsonUtility.FromJson<GameScore>(dataAsJson);
+            print(newGS);  // override Tostring
+        }
+        else
+        {
+            print("no File !!");
+        }
+    }
+
+    private void SaveJsonFile()
+    {
+        GameScore gs = new GameScore();
+        gs.level = 10;
+        gs.timeElapsed = 300;
+        gs.playerName = "DGA";
+        gs.dontCare = "dontcare";
+
+        Item item = new Item();
+        item.itemid = 1000;
+        item.iconImage = "image1.png";
+        item.price = 10000;
+
+        gs.items = new List<Item>();
+        gs.items.Add(item);
+
+        Item item1 = new Item();
+        item1.itemid = 2000;
+        item1.iconImage = "image2.png";
+        item1.price = 7777;
+
+        gs.items.Add(item1);
+
+        string dataAsJson = JsonUtility.ToJson(gs, true);  // 두번째 Parameter는 PrettyPrint 말그대로 줄바꿈을 해줘서 보기좋게 만들어준다
+        print(dataAsJson);
+        File.WriteAllText(pathJson, dataAsJson);        // Sync == Blocked 저장하는동안 멈춤        
+    }
+}
+
+[Serializable]
+public class GameScore
+{
+    public int level;
+    public float timeElapsed;
+    public string playerName;
+
+    [NonSerialized]
+    public string dontCare;
+
+    public List<Item> items;
+
+    public override string ToString()
+    {
+        return level + ", " + timeElapsed + ", " + playerName + ", " + items.Count;
+    }
+}
+
+[Serializable]
+public class Item
+{
+    public int itemid;
+    public string iconImage;
+    public int price;
+}
