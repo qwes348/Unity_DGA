2461a1b4284f53466448feaf7d8ba8624fb1339f
Hwngk <qwes348@naver.com>
2019-06-20 12:34:03 +0900

    DGA_Unity

---
+++ Day36_HTTP_Json/Assets/DownloadJson.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DownloadJson : MonoBehaviour
+{
+    // dummy json: https://jsonplaceholder.typicode.com/
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(GetJsonFromURL());
+    }
+
+    IEnumerator GetJsonFromURL()
+    {
+        string url = "https://jsonplaceholder.typicode.com/todos/1";
+
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
+            if (www.isNetworkError || www.isHttpError)
+            {
+                print(www.error);
+            }
+            else
+            {
+                string json = www.downloadHandler.text;
+                print(json);
+            }
+        }
+    }
+}
---
+++ Day37_LoadingScene_ProgressBar/Assets/Attack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    Animator anim;
+    //Movement movement;
+    [HideInInspector]
+    public bool isAttacking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        //movement = gameObject.transform.GetComponent<Movement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            StartCoroutine(Attacking());
+        }
+    }
+
+    IEnumerator Attacking()
+    {
+        anim.SetTrigger("OnAttack");
+        isAttacking = true;
+        yield return new WaitForSeconds(0.5f);
+        isAttacking = false;
+    }
+}
