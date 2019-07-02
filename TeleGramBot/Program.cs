5385471f900fdddd0ec62559d4f6e36f9648a2e7
Hwngk <qwes348@naver.com>
2019-07-02 17:55:59 +0900

    Unity_DGA

---
+++ TeleGramBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace TelegramBot {
+  class Program {
+    static ITelegramBotClient botClient;
+    static void Main() {
+        botClient = new TelegramBotClient("635435512:AAGFCHk0gb3ieFUZ5TTxTBEMuEmJD1htOn8");
+        var me = botClient.GetMeAsync().Result;
+        Console.WriteLine(
+            $"Hello, World! I am user {me.Id} and my name is {me.FirstName}."
+        );
+
+        botClient.OnMessage += Bot_OnMessage;
+        botClient.StartReceiving();
+        Thread.Sleep(int.MaxValue);
+    }
+
+    static async void Bot_OnMessage(object sender, MessageEventArgs e)  // like Coroutine
+    {
+        if(e.Message.Text != null)
+        {
+            Console.WriteLine($"Received a text message in chat {e.Message.Chat.Id}");
+            await botClient.SendTextMessageAsync(  // like yield return
+                chatId: e.Message.Chat,
+                text: "You said \n" + e.Message.Text
+            );
+        }
+    }
+  }
+}
